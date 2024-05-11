cbe0ae189bf28f9d590347e1c02034dfa9ae8008
Shion Sonozaki <sssyaon@gmail.com>
2024-05-11 10:07:26 +0700

    day07 done

--- src/day07/ex00/minCoins.go
+++ src/day07/ex00/minCoins.go
@@ -17,6 +17,16 @@ func MinCoins(val int, coins []int) []int {
 	return res
 }
 
+// MinCoins2 calculates the minimum number of coins needed to cover a given value using certain coin denominations.
+//
+// This implementation checks the boundary cases and sorts the coins in ascending order before processing them.
+//
+// Parameters:
+//   - val: The value that needs to be covered.
+//   - coins: The available denominations of coins.
+//
+// Returns:
+//   - []int: The minimum number of coins needed to make change, represented as a slice of coin values.
 func MinCoins2(val int, coins []int) []int {
 	if len(coins) == 0 || val == 0 {
 		return []int{}
@@ -37,3 +47,9 @@ func MinCoins2(val int, coins []int) []int {
 
 	return res
 }
+
+// Generate documentation:
+// To generate HTML documentation, execute the following command in your terminal:
+// godoc -http=:6060
+// Then open a web browser and go to http://localhost:6060/pkg/<package-name>
+// Replace <package-name> with the name of the package containing your code.
